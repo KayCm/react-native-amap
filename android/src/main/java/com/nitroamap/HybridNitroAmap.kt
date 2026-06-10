5f0e359ca5c7c8ee89d9021d630193f6860d7f36
KayCM <Rain@m1989.com>
2026-06-10 17:31:06 +0800

    android update

--- android/src/main/java/com/nitroamap/HybridNitroAmap.kt
+++ android/src/main/java/com/nitroamap/HybridNitroAmap.kt
@@ -6,11 +6,11 @@ import androidx.annotation.Keep
 import com.facebook.proguard.annotations.DoNotStrip
 import com.facebook.react.uimanager.ThemedReactContext
 import com.margelo.nitro.nitroamap.HybridNitroAmapSpec
-//import com.amap.api.maps.MapView
 import androidx.fragment.app.Fragment
 import android.view.ViewGroup
 import android.widget.FrameLayout
-//import com.amap.api.maps.MapView
+import com.amap.api.maps2d.MapView
+import com.amap.api.maps2d.AMap;
 
 @Keep
 @DoNotStrip
@@ -18,9 +18,17 @@ class HybridNitroAmap(val context: ThemedReactContext): HybridNitroAmapSpec() {
     // View
     //override val view: View = View(context)
 
-    override val view: View = FrameLayout(context)
+    //override val view: View = FrameLayout(context)
 
+   override val view:MapView = MapView(context).apply {
+                                                           layoutParams = FrameLayout.LayoutParams(
+                                                               FrameLayout.LayoutParams.MATCH_PARENT,
+                                                               FrameLayout.LayoutParams.MATCH_PARENT
+                                                           )
+                                                       }
 
+    //AMap aMap = view.getMap();
+    //aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
 
     // Props
     private var _isRed = false
